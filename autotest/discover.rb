ff304a605396383ab743c8d555293795d29511ef
Rob Westgeest <rob@qwan.it>
2011-03-23 10:41:25 +0100

    price added

--- autotest/discover.rb
+++
@@ -1,4 +0,0 @@
-# autotest/discover.rb
-require File.dirname(__FILE__) + '/testunit'
-
-Autotest.add_discovery { "testunit" }
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,25 +1,73 @@
 require 'enum'
 
 class VendingMachine
-    def initialize
-        @cans = {}
-        @cans.default = Can.none
+  def initialize
+    @cans = {}
+  end
+  
+  def set_value(v)
+    @c = v
+  end
+
+  # delivers the can if all ok
+  def deliver(choice)
+    res = nil
+    if @cans.has_key?(choice)
+      # check price
+      if @cans[choice].price == 0 
+        res = @cans[choice].type
+      # or price matches
+      elsif @c != nil 
+        if  @cans[choice].price <= @c
+          res = @cans[choice].type
+          @c -= @cans[choice].price
+        end
+      end
+    else
+      res = Can.none
     end
-    
-    def deliver(choice)
-        return @cans[choice]
+
+    # if can is set then return 
+    # otherwise we need to return the none
+    if (res == nil)
+      return Can.none
+    else 
+      return res
     end
-    
-    def configure(choice, can)
-        @cans[choice] = can
-    end
+  end
+  
+  def configure(choice, c, price = 0)
+    @price = price
+    can = CanContainer.new
+    can.type = c
+    can.price = price
+    @cans[choice] = can
+  end
+end
+
+class CanContainer
+  def type=(t)
+    @the_type = t
+  end
+
+  def type
+    return @the_type
+  end
+
+  def price=(p) 
+    @p = p
+  end
+
+  def price
+    @p
+  end
 end
 
 class Can < Enum
-    values :none, :cola
+  values :none, :cola, :fanta, :sprite
 end
 
 class Choice < Enum
-    values :cola, :fanta
+  values :cola, :fanta, :sprite, :beer
 end
 
